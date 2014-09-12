b6ca145b014b5ab1e8eca2cbb529278ebef1744c
Eric Mosher <emoshmosh@gmail.com>
2014-09-12 15:07:53 -0400

    feat(grunt): add grunt integration
    Create Gruntfile and added new npm dependencies. Grunt allows for automating tasks.

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+
+module.exports = function ( grunt ) {
+
+  grunt.loadNpmTasks('grunt-conventional-changelog');
+  grunt.loadNpmTasks('grunt-bump');
+
+  grunt.initConfig({
+    changelog: {
+      options: {
+        dest: 'CHANGELOG.md',
+        template: 'changelog.tpl'
+      }
+    },
+    bump: {
+      options: {
+        files: [
+          "package.json" // Add bower or others here
+        ],
+        commit: false,
+        commitMessage: 'chore(release): v%VERSION%',
+        commitFiles: [
+          "package.json"
+        ],
+        createTag: false,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: false,
+        pushTo: 'origin'
+      }
+    }
+  });
+  
+}
